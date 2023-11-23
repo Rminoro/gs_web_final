007aafdd7e5045cea223ee4f817f239ecbdb9bc0
rminoro <rminoro@outlook.com>
2023-11-23 20:11:56 -0300

    tentativa

--- src/app/api/cadastro/route.js
+++ src/app/api/cadastro/route.js
@@ -40,11 +40,11 @@ export async function POST(dados) {
             },
             body: JSON.stringify(dados),
         });
-
+        console.log(dados)
         if (response.ok) {
             // A resposta está no formato JSON, então você pode retornar os dados.
             const responseData = await response.json();
-
+            
             // Realize ações adicionais, se necessário, com os dados recebidos.
 
             console.log('Cadastro realizado com sucesso!', responseData);
--- src/app/informativos/cadastro/page.jsx
+++ src/app/informativos/cadastro/page.jsx
@@ -1,5 +1,77 @@
-"use client"
-import menuStyle from '@/components/styles/cadastro.scss'
+// "use client"
+// import menuStyle from '@/components/styles/cadastro.scss'
+// import React, { useState } from 'react';
+
+// const Cadastro = () => {
+//   const [id, setId] = useState('');
+//   const [nome, setNome] = useState('');
+//   const [idade, setIdade] = useState('');
+//   const [cpf, setCpf] = useState('');
+//   const [email, setEmail] = useState('');
+//   const [telefone, setTelefone] = useState('');
+
+//   const handleSubmit = async (e) => {
+//     e.preventDefault();
+
+//     const dadosCadastro = { id, nome, idade, cpf, email, telefone };
+
+//     try {
+//       const resposta = await fetch('http://localhost:3000/api/cadastro', {
+//         method: 'POST',
+//         headers: {
+//           'Content-Type': 'application/json; charset=UTF-8',
+//         },
+//         body: JSON.stringify(dadosCadastro),
+//       });
+//       console.log('Corpo da solicitação:', JSON.stringify(dadosCadastro));
+   
+//     } catch (erro) {
+//       console.error('Erro na requisição:', erro);
+//     }
+//   };
+
+//   return (
+//     <div >
+//       <h1>Cadastro de Paciente</h1>
+//       <form onSubmit={handleSubmit}>
+//       <label>
+//           Id:
+//           <input type="number" value={id} onChange={(e) => setId(e.target.value)} />
+//         </label>
+//         <label>
+//           Nome:
+//           <input type="text" value={nome} onChange={(e) => setNome(e.target.value)} />
+//         </label>
+//         <br />
+//         <label>
+//           Idade:
+//           <input type="number" value={idade} onChange={(e) => setIdade(e.target.value)} />
+//         </label>
+//         <br />
+//         <label>
+//           CPF:
+//           <input type="text" value={cpf} onChange={(e) => setCpf(e.target.value)} />
+//         </label>
+//         <br />
+//         <label>
+//           Email:
+//           <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+//         </label>
+//         <br />
+//         <label>
+//           Telefone:
+//           <input type="text" value={telefone} onChange={(e) => setTelefone(e.target.value)} />
+//         </label>
+//         <br />
+//         <button type="submit">Cadastrar</button>
+//       </form>
+//     </div>
+//   );
+// };
+
+// export default Cadastro;
+"use client";
+import menuStyle from '@/components/styles/cadastro.scss';
 import React, { useState } from 'react';
 
 const Cadastro = () => {
@@ -13,7 +85,14 @@ const Cadastro = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    const dadosCadastro = { id, nome, idade, cpf, email, telefone };
+    const dadosCadastro = {
+      id: parseInt(id),
+      nome,
+      idade: parseInt(idade),
+      cpf,
+      email,
+      telefone,
+    };
 
     try {
       const resposta = await fetch('http://localhost:3000/api/cadastro', {
@@ -24,17 +103,22 @@ const Cadastro = () => {
         body: JSON.stringify(dadosCadastro),
       });
 
-   
+      if (resposta.ok) {
+        console.log('Cadastro realizado com sucesso!');
+        // Realize ações adicionais, se necessário.
+      } else {
+        console.error('Erro ao cadastrar paciente:', resposta.status, resposta.statusText);
+      }
     } catch (erro) {
       console.error('Erro na requisição:', erro);
     }
   };
 
   return (
-    <div >
+    <div>
       <h1>Cadastro de Paciente</h1>
       <form onSubmit={handleSubmit}>
-      <label>
+        <label>
           Id:
           <input type="number" value={id} onChange={(e) => setId(e.target.value)} />
         </label>
