e756bf380a39a30e7dbf7d8d17a34869b12db992
rminoro <rminoro@outlook.com>
2023-11-23 23:43:24 -0300

    arrumando

--- src/app/menu/principal/page.jsx
+++ src/app/menu/principal/page.jsx
@@ -1,13 +1,13 @@
-import Menu from "@/components/menu/menu";
+// import Menu from "@/components/menu/menu";
 
-const principal = () =>{
-    return(
-    <div>
-      <Menu />
-      <h1>Welcome to the Home Page</h1>
-      {/* Conteúdo da sua página inicial */}
-    </div>
-  );
-};
+// const principal = () =>{
+//     return(
+//     <div>
+//       <Menu />
+//       <h1>Welcome to the Home Page</h1>
+//       {/* Conteúdo da sua página inicial */}
+//     </div>
+//   );
+// };
 
-export default principal;
+// export default principal;
